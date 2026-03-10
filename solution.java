e979ce01f36824f87f53f489b3d4fdc6a112b7f9
Aditya Bhardwaj <adityabhardwajpcm@gmail.com>
2026-03-10 20:06:28 +0530

    new code

---
+++ solution.java
@@ -0,0 +1,30 @@
+// Write a Java program to create a class called Vehicle with a method called drive(). Create a subclass called Car that overrides the drive() method to print "Repairing a car".
+
+
+ class Vehicle{
+    void drive()
+    {
+        System.out.println("i am driving");
+
+    }
+}
+
+
+class Car extends Vehicle
+{
+    @Override
+    void drive()
+    {
+        System.out.println("Repairing a car");
+
+    }
+}
+
+public class solution
+{
+    public static void main(String[] args)
+    {
+        Car c = new Car();
+        c.drive();
+    }
+}
