eb5901b2e5c7b1ef5f8471c85f94d1464ea9b20b
Aditya Bhardwaj <adityabhardwajpcm@gmail.com>
2026-03-12 20:56:26 +0530

    Add WorkSoln.java

---
+++ WorkSoln.java
@@ -0,0 +1,31 @@
+// //write a java progra, to create class called employee with called work() and getsalary() Create a subclass
+// called HrManager that overrides the work() method and adds a new method called addEmployee
+
+
+class Employee{
+    void work()
+    {
+        System.out.println("i work here")
+    }
+
+    void getSalary()
+    {
+        System.out.println("yay i got salary");
+    }
+}
+
+class HRManager extends Employee
+{
+    @Override
+    void work()
+    {
+        System.out.println("i work here in HRmanager")
+    }
+    void addEmployee()
+    {
+        System.out.print
+    }
+
+
+    }
+}
