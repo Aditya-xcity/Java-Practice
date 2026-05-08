7cb2d959f5aa959a03fb1a2cccc314c8a626becd
ADITYA BHARDWAJ <adityabhardwajpcm@gmail.com>
2026-05-08 21:18:20 +0530

    Add files via upload
    vehicle

---
+++ VehicleQuestion.java
@@ -0,0 +1,27 @@
+// Write a Java program to create a class called Vehicle with a method called start().
+// Create a subclass called Car that overrides the start() method to print a car start message.
+
+public class VehicleQuestion
+{
+    public static void main(String[] args)
+    {
+        // TODO: Create a Car object and call start().
+    }
+}
+
+class Vehicle
+{
+    void start()
+    {
+        // TODO: Write the general vehicle start message.
+    }
+}
+
+class Car extends Vehicle
+{
+    @Override
+    void start()
+    {
+        // TODO: Write the car start message.
+    }
+}
