97bd624a34396043a5999dd50b2d140ea2f1e834
ADITYA BHARDWAJ <adityabhardwajpcm@gmail.com>
2026-05-08 21:18:39 +0530

    Add files via upload

---
+++ SportQuestion.java
@@ -0,0 +1,27 @@
+// Write a Java program to create a class called Athlete with a method called compete().
+// Create a subclass called Swimmer that overrides the compete() method to print a swimming competition message.
+
+public class SportQuestion
+{
+    public static void main(String[] args)
+    {
+        // TODO: Create a Swimmer object and call compete().
+    }
+}
+
+class Athlete
+{
+    void compete()
+    {
+        // TODO: Write the general athlete compete message.
+    }
+}
+
+class Swimmer extends Athlete
+{
+    @Override
+    void compete()
+    {
+        // TODO: Write the swimming compete message.
+    }
+}
