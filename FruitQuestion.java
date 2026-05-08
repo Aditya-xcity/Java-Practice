e83ab579c20d25e2b921f1aca7632f345a395cf7
ADITYA BHARDWAJ <adityabhardwajpcm@gmail.com>
2026-05-08 21:17:31 +0530

    Add files via upload
    fruit question

---
+++ FruitQuestion.java
@@ -0,0 +1,32 @@
+// Write a Java program to create a class called Fruit with a method called taste().
+// Create a subclass called Apple that overrides the taste() method and adds a new method called nutritionInfo().
+
+public class FruitQuestion
+{
+    public static void main(String[] args)
+    {
+        // TODO: Create an Apple object and call taste() and nutritionInfo().
+    }
+}
+
+class Fruit
+{
+    void taste()
+    {
+        // TODO: Write the general fruit taste message.
+    }
+}
+
+class Apple extends Fruit
+{
+    @Override
+    void taste()
+    {
+        // TODO: Write the apple taste message.
+    }
+
+    void nutritionInfo()
+    {
+        // TODO: Write the apple nutrition information message.
+    }
+}
